78057625863dde67df9eba9cd1f09c06dd62c6b6
cangle007 <cang.b.le@gmail.com>
2024-11-21 15:16:45 +0700

    added keyboard

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,9 +1,22 @@
 import { useState } from 'react';
 import classNames from 'classnames';
 import styles from './Keyboard.module.scss';
+import alphabet from '../../data/alphabet.json';
 
 const Keyboard = () => {
-  return <div className={classNames(styles.root)}>Keyboard</div>;
+  return (
+    <div className={classNames(styles.root)}>
+      <div className={classNames(styles.border)}>
+        {alphabet.map((letter, i) => {
+          return (
+            <div className={classNames(styles.letterItem)} key={i}>
+              {letter}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Keyboard;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import classNames from 'classnames';
 import styles from './Main.module.scss';
 
+import HangmanDrawing from '../HangmanDrawing/HangmanDrawing';
+import HangmanWord from '../HangmanWord/HangmanWord';
+import Keyboard from '../Keyboard/Keyboard';
+
 import words from '../../data/words.json';
 
 type wordToGuestType = string;
@@ -15,9 +19,11 @@ const Main = () => {
   return (
     <div className={classNames(styles.root)}>
       <h1>Hangman Game</h1>
-
       <div className={classNames(styles.winLose)}>LOSE WIN</div>
       <p>{wordToGuess}</p>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
 
       <button
         onClick={() => {
